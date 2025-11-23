9c08ac5d9017a986c4b41e7c17acdc41e402c032
wbvink <wbvink@gmail.com>
2025-11-23 18:15:30 +0100

    Wordcount function added, counts Frankenstein

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def get_book_text(book_path) -> str:
+def get_book_text(book_path: str) -> str:
     """Read file (expects plaintext format) and return contents as string"""
     with open(book_path) as bookfile:
         book_contents = bookfile.read()
     return book_contents
 
+def wordcount(input_string: str) -> int:
+    """Count number of words in input string"""
+    wordlist = input_string.split() # use standard 'smart' sep
+    return len(wordlist) 
+
 def main():
-    """Read Frankenstein and print it to the console"""
+    """Read Frankenstein and print wordcount"""
     frank_str = get_book_text("books/frankenstein.txt")
-    print(frank_str)
+    book_wordcount = wordcount(frank_str)
+
+    print(f"Found {book_wordcount} total words")
 
 main()
