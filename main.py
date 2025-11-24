518cbba4cd371fac2b4a10a6aa5ded82f57f722e
wbvink <wbvink@gmail.com>
2025-11-24 12:31:07 +0100

    Sort character count and tidy reporting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import wordcount, char_count
+from stats import wordcount, char_count, sort_char_count
 
 def get_book_text(book_path: str) -> str:
     """Read file (expects plaintext format) and return contents as string"""
@@ -8,12 +8,23 @@ def get_book_text(book_path: str) -> str:
 
 def main():
     """Read Frankenstein and print wordcount"""
-    frank_str = get_book_text("books/frankenstein.txt")
+    frank_path = "books/frankenstein.txt"
+    frank_str = get_book_text(frank_path)
     # wordcount:
     book_wordcount = wordcount(frank_str)
-    print(f"Found {book_wordcount} total words")
     # character count:
     book_char_count = char_count(frank_str)
-    print(book_char_count)
+    book_sorted_char_count = sort_char_count(book_char_count) # convert to list and sort
+    for di in book_sorted_char_count: # remove all non-letter characters from list
+        if not di["char"].isalpha():
+            book_sorted_char_count.remove(di)
+    # create report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frank_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_wordcount} total words")
+    print("--------- Character Count -------")
+    for di in book_sorted_char_count:
+        print(f"{di["char"]}: {di["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -13,5 +13,22 @@ def char_count(input_string:str) -> dict:
     # Fill dictionary
     for ch in unique_char:
         output_dict[ch] = lc_string.count(ch) # start with 1 for debugging 
-
     return output_dict
+
+def dict_char_count_sort_helper(items: dict) -> int:
+    """Helper function to sort_char_count: takes a dictionary and returns the value of the "num" key
+    """
+    return items["num"]
+
+def sort_char_count(input_dict:dict) -> list:
+    """Convert input-dictionary of character counts to list of dictionaries sorted in descending order of occurrence
+    Input: dictionary with character:occurrences format, e.g. {'p': 6121, 'r': 20818, 'o': 25225, ...
+    Output: list of dictionaries where each dict has this format: {"char": "b", "num": 4868}
+    """
+    output_list = [] # initiate empty list
+    # Translate to new format
+    for ch in input_dict:
+        output_list.append({"char":ch, "num":input_dict.get(ch)})
+    # Sort to descending occurrence order
+    output_list.sort(reverse=True, key=dict_char_count_sort_helper)
+    return output_list
